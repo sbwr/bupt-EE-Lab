ee07560b87ccde1071431d5a2dff9078daf67b9b
sbwr <18600240302>
2021-04-24 20:39:38 +0800

    作业2

---
+++ Embedded-Arm7/homework/work2/work2.s
@@ -0,0 +1,24 @@
+; r0-r3赋初值
+; r1r0, r3r2分别连接为两个64位数
+mov  r1, #0
+mov  r0, #0
+mov  r3, #0
+mov  r2, #0xFFFFFFFF
+
+LOOP:
+    bl  add64       ; 完成一次加法
+    sub r2, r2, #1  ; 第二个加数-1
+    cmp r2, #0
+    bne LOOP        ; 条件跳转, 看CPSR标志位
+
+; 结束程序
+nop
+nop
+nop
+
+; 64位加法 r1r2 + r3r4
+; r1r2为计算结果
+add64:
+    adds r0, r0, r2
+    adc  r1, r1, r3
+    mov  pc, lr     ; 子程序结束，回到主程序
