46704bf74f139966abdd76cf025e445bcdaeb067
sbwr <18600240302>
2021-04-26 20:52:48 +0800

    done homework0418

--- Microcomputer/homework_0418/work3.asm
+++ Microcomputer/homework_0418/work3.asm
@@ -1,40 +1,47 @@
-; 假设从变量buff开始存放了200个字，编写一个程序统计出其正数、0和负数的个数，并把它们分别存入n1、n2和n3中。
+; 假设从变量buff开始存放了200个字，编写一个程序统计出其正数、0和负数的个数，
+; 结果分别存入n1、n2和n3中。
 .model small
 .stack 100h
-.data                           ; 定义数据段
-BUFF DW 102 DUP(1),54 DUP(0),44 DUP(-1) 
-.const                          ; 定义常数段（可选）
-.code                           ; 定义代码段
+.data
+buff dw 70 dup (-2), 64 dup(0), 66 dup(2)
+n1 dw 0
+n2 dw 0
+n3 dw 0
+.const
+.code
 start:
 main proc far
     mov ax, @data
     mov ds, ax
+    lea si, buff
+    mov cx, 200         ; 因为先判断跳出循环，所以cx初始值应为buff长度+1
+    
+    lp: 
+        dec cx
+        jz exit
+        mov ax, word ptr [si]
+        inc si
+        inc si
+        cmp ax, 0       ; 当前字(有符号数)和0比较，计算[si]-0，改变SF ZF
+        jz zero         ; ZF=1, 则为0
+        js negtive      ; SF=1, 则为负数
+        jns positive    ; ZF,SF=0 则为正数
 
-    head: ; =========寻找字符串头部的空格==========
-        mov ah,[si]             ; 将偏移量为si的值赋给ah
-        inc si                  ; 偏移量递增
-        cmp ah, ' '             ; 
-        jz  movehead            ; 找到空格则zf=1，在movehead中将字符串起始位置后移
-        jnz rear                ; 头部空格全部找到，开始找尾部
+    zero: ; n2++
+        inc word ptr [n2]
+        jmp lp
 
-    movehead: ; =======改变字符串起始偏移地址，存在bx中=======
-        mov bx,si               ; 存放新的字符串起始地址
-        jmp head
+    negtive: ; n3++
+        inc word ptr [n3]
+        jmp lp
 
-    rear: ; =========寻找字符串尾部的空格==========
-        mov ah,[di]             ; 将偏移量为si的值赋给ah
-        dec di                  ; 偏移量递增
-        cmp ah, ' '             ; 
-        jz  moverear            ; 找到空格则zf=1，在moverear中改变结尾位置
-        jnz exit                ; 尾部空格全部找到，结束
-
-    moverear: ; =======改变字符串末尾位置，' '->'0'=======
-        mov byte ptr [di],'0'
-        jmp rear
+    positive: ; n1++
+        inc word ptr [n1]
+        jmp lp
 
     exit: ; ============退出程序============
-        mov si,bx               ; 将新的起始位置赋给si
         MOV	AH,4CH
         INT 21H
+
 main endp    
 end start
