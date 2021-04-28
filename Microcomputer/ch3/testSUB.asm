9c7d6b1e7b5196ef5fda02f5eff53fcbc7a48bd1
sbwr <18600240302>
2021-04-28 17:46:37 +0800

    更新 实验一等

--- Microcomputer/ch3/testSUB.asm
+++ Microcomputer/ch3/testSUB.asm
@@ -1,4 +1,5 @@
 ; 探索sub,sbb,cmp等减法的用法
+; 观察flag
 .model small
 .data                   ;定义数据段
 array db 100 dup (0)
@@ -6,17 +7,23 @@ array db 100 dup (0)
 .code                   ;定义代码段
 start:
     mov ax, @data
-    mov ds, AX
+    mov ds, ax
 
     ; test cmp: dst - src
+    ; unsigned
     mov ax, 0aaaah
     mov bx, 0
     cmp ax, bx  ; dst > src 时cf=0
     cmp bx, ax  ; dst < src 时cf=1
     cmp bx, 0   ; dst, src 操作数相等时cf=0, zf=1
 
+    ; signed
+    cmp bx, -1
+    mov ax, -0aaaah
+    cmp ax, 0
+
+
     ; exit
-    ; 不知道发生了什么反正这样正常退出了
     MOV	AH,4CH
 	INT 21H
 end start
---
+++ Microcomputer/ch3/testprint.asm
@@ -0,0 +1,4 @@
+; 使用int 21h中断输入输出
+.model small
+.stack 100h
+.data
